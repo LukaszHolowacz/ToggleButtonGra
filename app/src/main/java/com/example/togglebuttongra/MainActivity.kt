db66b3acd32d7ca8ed7377ac4232ed06d7676bb4
Lukas <lukasz.holowacz5@gmail.com>
2022-11-15 22:56:21 +0100

    aa

--- app/src/main/java/com/example/togglebuttongra/MainActivity.kt
+++ app/src/main/java/com/example/togglebuttongra/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.togglebuttongra
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.TextKeyListener.clear
 import android.widget.Toast
 import android.widget.ToggleButton
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var punkty = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,22 +28,24 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.btn9)
         )
 
-
-
         val numbers: List<Int> = listOf(9,8,7,6,5,4,3,2,1)
 
-        for (i in 0..8){
+        for (i in 0..przyciski.size-1){
             przyciski[i].setOnClickListener {
                 if(i+1 != numbers[pom]){
-                    for (j in 0..8){
+                    for (j in 0..przyciski.size-1){
                         przyciski[j].isChecked = false
+                        przyciski[j].setBackgroundResource(R.drawable.buttonback)
                     }
                     pom = 0
                 }
                 else{
                     pom+=1
                 }
-                zmiana_tla(przyciski[0])
+                zmiana_tla(przyciski[i])
+                if(pom == numbers.size-1){
+
+                }
             }
         }
     }
