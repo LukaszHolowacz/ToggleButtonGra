95745835517c53c7400e426b6bd9afd7ab7fc44b
Lukas <lukasz.holowacz5@gmail.com>
2022-11-21 17:33:35 +0100

    do zrobienia tylko losowanie kolejności przycisków

--- app/src/main/java/com/example/togglebuttongra/MainActivity.kt
+++ app/src/main/java/com/example/togglebuttongra/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.togglebuttongra
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.TextKeyListener.clear
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
 import kotlin.random.Random
@@ -15,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
         var pom = 0
+        var score = sharedPreferences.getInt("score", 0)
         val przyciski = listOf<ToggleButton>(
             findViewById(R.id.btn1),
             findViewById(R.id.btn2),
@@ -27,24 +31,32 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.btn8),
             findViewById(R.id.btn9)
         )
+        val numbers = losuj_kolejnosc()
 
-        val numbers: List<Int> = listOf(9,8,7,6,5,4,3,2,1)
+        findViewById<TextView>(R.id.textView).text = "Score: " + score.toString()
 
         for (i in 0..przyciski.size-1){
             przyciski[i].setOnClickListener {
                 if(i+1 != numbers[pom]){
-                    for (j in 0..przyciski.size-1){
-                        przyciski[j].isChecked = false
-                        przyciski[j].setBackgroundResource(R.drawable.buttonback)
-                    }
+                    reset_przyciskow(przyciski)
                     pom = 0
+                    Toast.makeText(applicationContext, "Coś źle", Toast.LENGTH_SHORT).show()
                 }
                 else{
                     pom+=1
                 }
                 zmiana_tla(przyciski[i])
-                if(pom == numbers.size-1){
-
+                if(pom == numbers.size){
+                    pom=0
+                    score += 1
+                    findViewById<TextView>(R.id.textView).text = "Score: " + score.toString()
+                    reset_przyciskow(przyciski)
+                    losuj_kolejnosc()
+                    Toast.makeText(applicationContext, "Wylosowałem nową kolejność, Spróbuj teraz!", Toast.LENGTH_SHORT).show()
+                    val editor = sharedPreferences.edit()
+                    editor.apply{
+                        putInt("score", score)
+                    }.apply()
                 }
             }
         }
@@ -57,5 +69,17 @@ class MainActivity : AppCompatActivity() {
         else{
             przycisk.setBackgroundResource(R.drawable.buttonback)
         }
+    }
+
+    fun reset_przyciskow(przyciski: List<ToggleButton>){
+        for (j in 0..przyciski.size-1){
+            przyciski[j].isChecked = false
+            przyciski[j].setBackgroundResource(R.drawable.buttonback)
+        }
+    }
+
+    fun losuj_kolejnosc(): List<Int> {
+        val lista: List<Int> = listOf(9,8,7,6,5,4,3,2,1)
+        return lista
     }
 }
